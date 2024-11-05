dc8b76439e7e67bb5e15038a5ef117954c781dd9
Monika100792 <Monikadg4@gmail.com>
2024-11-05 15:59:48 +0530

    Add files via upload

---
+++ Attribute.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const name="Dheeraj";
+const img1="https://qwertytrewq";
+const link="http://jhgfdfghjkhgf";
+ReactDOM.render (
+    <>
+    <h1 contentEditable="true">My name is{name}</h1>
+    <img src={img1}alt="randomimages"/>
+    <a href={link}target="_dattu">
+       <imgsrc={img1} alt="randomimages"/>
+    </a>
+    </>
+    ,document.getElementById("root"));
